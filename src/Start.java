77faf4e345df60fd393eba0b3a64261dfa4a9e62
KNAXtryhaaard <57808426+KNAXtryhaaard@users.noreply.github.com>
2021-06-28 07:43:55 +0200

    changees von nOaH

--- src/Start.java
+++ src/Start.java
@@ -2,8 +2,7 @@ public class Start {
     public static void main(String[] args) {
         System.out.println("game started");
         MainMenu mainMenu = new MainMenu();
-        Thread t = new Thread(mainMenu);
-        t.start();
+        new Thread(mainMenu).start();
     }
 }
 
